c31f21b9c7d70210b26525a1e7453dd67b7d3627
donCallm <nikitich-1998@bk.ru>
2023-07-30 17:20:05 +0300

    [HOTFIX] add and use submodul

---
+++ build/CMakeFiles/CppSolyanka.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppSolyanka.dir/src/main.cpp.o"
+  "CMakeFiles/CppSolyanka.dir/src/main.cpp.o.d"
+  "CppSolyanka"
+  "CppSolyanka.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppSolyanka.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
+#include <string>
+#include <fstream>
+#include <nlohmann/json.hpp>
+using json = nlohmann::json;
 
 int main()
 {
-    std::cout << "Hello, world";
-
+    json j {};
+    std::ifstream json_file_stream("/home/nikita/CppSolyanka/src/HelloWorld.json");
+    std::cout << (j = json::parse(json_file_stream))["hello"] << std::endl;
+    
     return 0;
 }
